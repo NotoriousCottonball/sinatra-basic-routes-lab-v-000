3a6f0f52df540c961f333e7a5278925146066752
George  Pianka <gpianka1@gmail.com>
2018-11-16 12:27:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,9 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-get ('/name'){"My name is George"}
+get ('/name'){"My name is George."}
 
+get ('/hometown'){"My hometown is Pocantico Hills."}
 
+get ('/favorite-song'){"My favorite song is "
 
 
 end
